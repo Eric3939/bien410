238e63a1352e80d50b8e6b0bc15e4958327fbe69
Eric3939 <sunyitao123@gmail.com>
2024-11-17 20:25:16 -0500

    subprocess

---
+++ BIEN410_FinalProject/testing/test.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.run(['python3', 'testing.py', '-p', '../src5/outfile.txt', '-l', '../training_data/labels.txt'])
--- practice/mlp3.py
+++ practice/mlp3.py
@@ -113,7 +113,6 @@ if __name__ == "__main__":
     mlp = MLP(20, [15, 10, 5], 1)
     # mlp.fit(X_train, y_train, epochs=6000, rate=0.1)
     # mlp.save_parameters('param.txt')
-
     mlp.load_parameters('param.txt')
     y_pred = mlp.predict(X_test)
     
