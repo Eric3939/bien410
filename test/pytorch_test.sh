6b81595b9b8cb358868c372c4964eb158c7518c8
Eric3939 <sunyitao123@gmail.com>
2024-11-19 23:44:41 -0500

    pytorch test

--- test/pytorch_test.sh
+++ test/pytorch_test.sh
@@ -3,13 +3,17 @@
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=00:01:00
-#SBATCH --output=%N-%j.out
+#SBATCH --mail-user=yitao.sun@mail.mcgill.ca
+#SBATCH --mail-type=ALL
 
 module load python # Make sure to choose a version that suits your application
 # virtualenv --no-download $SLURM_TMPDIR/env
 # source $SLURM_TMPDIR/env/bin/activate
 # pip install torch --no-index
 
-source ../bien410/bin/activate
+cd ~/$project/
+source ~/bien410/bien410/bien410/bin/activate
+cd ~/bien410/bien410/test/
+
 
 python pytorch-test.py
