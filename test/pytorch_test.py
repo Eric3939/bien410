b23c4b27b31315bea2848565b1a49f36f1b40222
Eric3939 <sunyitao123@gmail.com>
2024-11-22 21:35:03 -0500

    test pytorch_test.py

--- test/pytorch_test.py
+++ test/pytorch_test.py
@@ -4,6 +4,8 @@ print(x)
 y = torch.rand(5, 3)
 print(y)
 # let us run the following only if CUDA is available
+print(torch.cuda.device_count())  # Number of GPUs available
+print(torch.cuda.get_device_name(0))  # Name of the first GPU (if available)
 if torch.cuda.is_available():
     x = x.cuda()
     y = y.cuda()
